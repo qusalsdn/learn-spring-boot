3f63fac9da807ccd19a081a9deae49de34541c73
변민우 <yja05092@gmail.com>
2024-04-02 18:45:44 +0900

    Profile & ConfigurationProperties

---
+++ src/main/java/com/qusalsdn/springboot/learnspringboot/CurrencyConfigurationController.java
@@ -0,0 +1,19 @@
+package com.qusalsdn.springboot.learnspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController // RestAPI를 이용하기 위한 선언
+public class CurrencyConfigurationController {
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retrieveAllCourses() {
+        return configuration;
+    }
+}
